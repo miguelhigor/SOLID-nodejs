4b642286606c98eb8729a6609298af34d1ffde1b
Higor Miguel <higor.mig@gmail.com>
2021-05-17 10:17:21 -0300

    List all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,19 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.headers;
+        const id: string = user_id as string;
+
+        try {
+            const users = this.listAllUsersUseCase.execute({ user_id: id });
+            return response.status(200).json(users);
+        } catch (error) {
+            return response.status(400).json({ error: error.message });
+        }
+    }
 }
 
 export { ListAllUsersController };
